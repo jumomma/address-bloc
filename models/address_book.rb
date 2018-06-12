e1d17e7de0bfb7e3e5b3ed5ba8b7cb076e62cd2f
jumomma <jumomma@gmail.com>
2018-06-12 10:29:25 -0400

    assignment: searching complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -73,4 +73,12 @@ require "csv"
       return nil
     end
 
+    def iterative_search(name)
+      entries.each do |entry|
+        if entry.name == name
+          return entry
+        end
+      end
+      return nil
+    end
 end
